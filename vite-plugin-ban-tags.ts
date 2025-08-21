3826a22fe1a611cb47eecc44975847a69dee36ed
Lam Nguyen <la.nguyen@f5.com>
2025-08-20 21:03:30 -0700

    fix: Resolved plugin not working on build due to incomplete regex

--- vite-plugin-ban-tags.ts
+++ vite-plugin-ban-tags.ts
@@ -1,15 +1,15 @@
 import type { Plugin } from 'vite';
 
 
-const fileRegex: RegExp = /!\/\.(ts|tsx|js|jsx)$\//
-const aCodeRegex: RegExp = /jsxDEV\(\s*["']a["']/
-const banList: RegExp[] = [aCodeRegex]
+const fileRegex: RegExp = /!\/\.(ts|tsx|js|jsx)$\// /* Note: Might be an issue with node_modules since it isn't ignored when transforming */
+const aCodeRegex: RegExp = /jsx(?:DEV)?\(\s*["']a["']/;
+const banList: RegExp[] = [aCodeRegex];
 
 export default function banTagsPlugin(): Plugin {
     return {
         name: "no-a-tag-plugin",
         transform(code: string, id: string) {
-            if(fileRegex.test(id)) return
+            if(fileRegex.test(id)) return;
             
             for(const item of banList) {
                 if(item.test(code)){
