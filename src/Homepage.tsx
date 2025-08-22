c0daca71e78212e62e477800318060eac56e9fb5
Lam Nguyen <la.nguyen@f5.com>
2025-08-22 10:24:24 -0700

    feat: Refactored homepage

--- src/Homepage.tsx
+++ src/Homepage.tsx
@@ -1,33 +1,9 @@
-import InteriorColorSelector from "@/components/InteriorColorSelector/InteriorColorSelector"
-import { BACKGROUND_COLOR_CLASSES, DEFAULT_BG_COLOR } from "@/constants/InteriorColorConstants";
-import { InteriorColorProvider } from '@/context/InteriorColorContext';
-import { useInteriorColor } from "@/hooks/useInteriorColor";
-
-function HomepageProviders({ children }: { children: React.ReactNode }) {
-    return (
-        <InteriorColorProvider>
-            { children }
-        </InteriorColorProvider>
-    )
-}
-
 function Homepage() {
   return (
-    <HomepageProviders>
-      <HomepageContent />
-    </HomepageProviders>
+    <div className="h-screen">
+      
+    </div>
   )
-}
-
-function HomepageContent() {
-    const { interiorColor } = useInteriorColor();
-    const backgroundColor = BACKGROUND_COLOR_CLASSES[interiorColor.toLowerCase()] || DEFAULT_BG_COLOR
-
-    return (
-        <div className={`${backgroundColor} h-screen`}>
-            <InteriorColorSelector />
-        </div>
-    )
 }
 
 export default Homepage
