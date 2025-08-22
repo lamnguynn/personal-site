4f23b890e4b0fe4e875a9faffa9eeb8953c3b2e5
Lam Nguyen <la.nguyen@f5.com>
2025-08-21 20:36:54 -0700

    chore: Removed the config file + added safeList to index.css

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    content: ['./src/**/*.{js,ts,jsx,tsx}'],
-    safelist: [
-      'bg-interior-tartufo',
-      'bg-interior-silverstone',
-      'bg-interior-fjord',
-    ],
-};
