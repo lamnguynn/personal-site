5ba040dc8c7aeec49ab2dd216b61bcb3017ba330
Lam Nguyen <la.nguyen@f5.com>
2025-08-21 19:47:36 -0700

    feat: Add file alias to vite + typescript

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,17 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import banTagsPlugin from './vite-plugin-ban-tags';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), banTagsPlugin()],
   server: {
     port: 3001,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, 'src'),
+    }
   }
 })
