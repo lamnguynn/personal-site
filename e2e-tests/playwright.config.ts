5b0f802d1c9575e76b4f2e2b8091b290c862bbfc
Lam Nguyen <33523248+lamnguynn@users.noreply.github.com>
2025-09-21 01:44:03 -0700

    feat: Add playwright

---
+++ e2e-tests/playwright.config.ts
@@ -0,0 +1,38 @@
+import { defineConfig, devices } from '@playwright/test';
+
+const BASE_URL = 'http://localhost:3001';
+export default defineConfig({
+  testDir: './src',
+  fullyParallel: true,
+  outputDir: './test-results/',
+  reporter: [['html', { outputFolder: './playwright-report' }]],
+  use: {
+    baseURL: BASE_URL,
+    trace: 'on-first-retry',
+    video: 'retain-on-failure',
+    screenshot: 'only-on-failure',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'webkit',
+      use: { ...devices['Desktop Safari'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+    {
+      name: 'Mobile Safari',
+      use: { ...devices['iPhone 15 Pro'] },
+    },
+  ],
+  webServer: {
+    command: 'npm run dev',
+    url: BASE_URL,
+    reuseExistingServer: !process.env.CI,
+  },
+});
